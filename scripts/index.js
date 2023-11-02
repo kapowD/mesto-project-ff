8b96337e648d3349a610ab0c3943bc03ebf0c521
KapowD <mikael99988@gmail.com>
2023-11-02 20:48:30 +0700

    PR5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+import { initialCards } from "./cards";
 
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+const content = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+const createCard = (element) => {
+  const card = content.querySelector(".places__item").cloneNode(true);
+  const cardTitle = card.querySelector(".card__title");
+  const cardImage = card.querySelector(".card__image");
+  const cardDelete = card.querySelector(".card__delete-button");
 
+  cardTitle.textContent = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+
+  cardDelete.addEventListener("click", deleteCard);
+  return card;
+};
+function deleteCard() {
+  //   const target = evt.target;
+  //   const card = target.closest('.card');
+  card.remove();
+}
+
+const renderCard = (cardElement) => {
+    placesList.append(cardElement);
+}
+
+initialCards.forEach((card) => {
+    renderCard(createCard(card));
+});
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
